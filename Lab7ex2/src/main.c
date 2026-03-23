093b98a3b9cf0cf621d20653f0daa2d7b7956f53
danasherif06-wq <danasherif06@gmail.com>
2026-03-23 16:56:47 +0400

    Lab 7 ex 2

--- Lab7ex2/src/main.c
+++ Lab7ex2/src/main.c
@@ -5,28 +5,28 @@
 
 void app_main(void)
 {
-    // -------- OUTPUT (LED) --------
+  
     gpio_reset_pin(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
-    // -------- INPUT (BUTTON) --------
+   
     gpio_reset_pin(BUTTON_PIN);
     gpio_set_direction(BUTTON_PIN, GPIO_MODE_INPUT);
-    gpio_pullup_en(BUTTON_PIN);  // as shown in your table
+    gpio_pullup_en(BUTTON_PIN);  
 
     while (1)
     {
-        // Read button state
+     
         int state = gpio_get_level(BUTTON_PIN);
 
         if (state == 0)
         {
-            // Button pressed (because pull-up → pressed = 0)
+            
             gpio_set_level(LED_PIN, 1);
         }
         else
         {
-            // Button not pressed
+        
             gpio_set_level(LED_PIN, 0);
         }
     }
