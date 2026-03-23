b1181b7968f9f3ce0c93667d29b066758f15f1f8
Dana <danasherif06@gmail.com>
2026-03-23 16:33:18 +0400

    Lab 7 exercise 1

---
+++ Lab7ex1/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+static const char *TAG = "LAB1";
+void app_main(void)
+{
+while (1) {
+ESP_LOGI(TAG, "Hello from ESP-IDF!");
+vTaskDelay(pdMS_TO_TICKS(1000));
+}
+}
